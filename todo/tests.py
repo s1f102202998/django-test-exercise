54fc132a7ac90578a9b25590517e854811b12cc9
1F10220299 <s1f102202998@.iniad.org>
2023-07-06 15:10:56 +0900

    コードを修正2

--- todo/tests.py
+++ todo/tests.py
@@ -4,9 +4,11 @@ from datetime import datetime
 from todo.models import Task
 
 # Create your tests here.
+
+
 class SampleTestCase(TestCase):
-  def test_sample1(self):
-    self.assertEqual(1 + 2, 3)
+      def test_sample1(self):
+              self.assertEqual(1 + 2, 3)
 
 
 class TaskModelTestCase(TestCase):
