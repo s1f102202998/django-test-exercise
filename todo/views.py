db41f3a8baac0a9808578965326e35a23d6e58a9
1F10220295 <s1f102202954@iniad.org>
2023-07-20 14:43:22 +0900

    2.TABインデントからスペースインデントに変更

--- todo/views.py
+++ todo/views.py
@@ -8,31 +8,31 @@ from todo.models import Task
 
 
 def index(request):
-	if request.method == 'POST':
-		task = Task(title=request.POST['title'], due_at=make_aware(parse_datetime(request.POST['due_at'])))
-		task.save()
+    if request.method == 'POST':
+        task = Task(title=request.POST['title'], due_at=make_aware(parse_datetime(request.POST['due_at'])))
+        task.save()
 
-	if request.GET.get('order') == 'due':
-		tasks = Task.objects.order_by('due_at')
-	else:
-		tasks = Task.objects.order_by('-posted_at')
+    if request.GET.get('order') == 'due':
+        tasks = Task.objects.order_by('due_at')
+    else:
+        tasks = Task.objects.order_by('-posted_at')
 
-	context = {
-		'tasks': tasks
-	}
-	return render(request, 'todo/index.html', context)
+    context = {
+        'tasks': tasks
+    }
+    return render(request, 'todo/index.html', context)
 
 
 def detail(request, task_id):
-	try:
-		task = Task.objects.get(pk=task_id)
-	except Task.DoesNotExist:
-		raise Http404("Task does not exist")
+    try:
+        task = Task.objects.get(pk=task_id)
+    except Task.DoesNotExist:
+        raise Http404("Task does not exist")
 
-	context = {
-		'task': task,
-	}
-	return render(request, 'todo/detail.html', context)
+    context = {
+        'task': task,
+    }
+    return render(request, 'todo/detail.html', context)
 
 
 def update(request, task_id):
@@ -41,7 +41,7 @@ def update(request, task_id):
 
     except Task.DoesNotExist:
         raise Http404("Task does not exist")
-    
+
     if request.method == 'POST':
         task.title = request.POST['title']
         task.due_at = make_aware(parse_datetime(request.POST['due_at']))
@@ -64,10 +64,10 @@ def delete(request, task_id):
 
 
 def close(request, task_id):
-	try:
-		task = Task.objects.get(pk=task_id)
-	except Task.DoesNotExist:
-		raise Http404("Task does not exist")
-	task.completed = True
-	task.save()
-	return redirect(index)
+    try:
+        task = Task.objects.get(pk=task_id)
+    except Task.DoesNotExist:
+        raise Http404("Task does not exist")
+    task.completed = True
+    task.save()
+    return redirect(index)
